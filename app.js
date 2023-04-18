2c07b0d8c11a4948ffc8ade3a2dff8d2676c4c8d
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-04-18 21:45:37 +0700

    feat: add a script for random color

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const board = document.querySelector('#board');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
 const SQUARES_NUMBER = 500;
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
@@ -19,10 +20,19 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
 
 // установка цвета при наведении
 function setColor(element) {
-  element.style.backgroundColor = 'red';
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
 // удаление цвета при уходе с квадрата
 function removeColor(element) {
   element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #000';
+}
+
+// задание случайного цвета из массива цветов
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
 }
