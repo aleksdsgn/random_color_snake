27e8f78e3e178f1dc68f3a4fc0463ecb18e5eed0
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-04-20 21:44:12 +0700

    refactor

--- app.js
+++ app.js
@@ -7,32 +7,29 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
   
-  square.addEventListener('mouseover', () => {
-    setColor(square);
-  })
+  square.addEventListener('mouseover', setColor)
   
-  square.addEventListener('mouseleave', () => {
-    removeColor(square);
-  })
+  square.addEventListener('mouseleave', removeColor)
 
   board.append(square);
 }
 
 // установка цвета при наведении
-function setColor(element) {
+function setColor(event) {
+  const element = event.target;
   const color = getRandomColor();
   element.style.backgroundColor = color;
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
 // удаление цвета при уходе с квадрата
-function removeColor(element) {
+function removeColor(event) {
+  const element = event.target;
   element.style.backgroundColor = '#111';
   element.style.boxShadow = '0 0 2px #000';
 }
 
 // задание случайного цвета из массива цветов
 function getRandomColor() {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
